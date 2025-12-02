4d1e3b94e992c75af9a893c1d8894c9115978ca5
Yiannis <ioannis.roussos@hotmail.com>
2025-12-02 17:00:07 +0200

    reconstructed project

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -15,7 +15,7 @@ const CustomButton = ({title, type, customStyles, handleClick}) => {
     }
 
   return (
-    <button className={`px-2 py-1.5 flex-1 rounded-md ${customStyles} `} style={generateStyle(type)} onClick={handleClick}>
+    <button className={` px-2 py-1.5 flex-1 rounded-md ${customStyles} `} style={generateStyle(type)} onClick={handleClick}>
       {title}
     </button>
   )
